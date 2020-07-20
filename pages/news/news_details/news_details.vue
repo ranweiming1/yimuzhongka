884d17260c6ef4523cb319ec4f3380e7d63dfe20
yanan990329 <1431796149@qq.com>
2020-07-20 10:46:19 +0800

    新闻资讯页面

--- pages/news/news_details/news_details.vue
+++ pages/news/news_details/news_details.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		<view class="top">
+		<!-- <view class="top">
 			<view class="textBox">
 				<text>资讯详情</text>
 			</view>
@@ -8,8 +8,8 @@
 				<image src="../../../static/icon_18.png" mode=""></image>
 			</view>
 		</view>
-		
-		
+		 -->
+
 		<view class="neir">
 			<view class="title">
 				<text>{{title}}</text>
@@ -21,11 +21,18 @@
 				<text>浏览量：{{description}}</text>
 			</view>
 		</view>
+		<view class="img">
+			<image :src="image" mode=""></image>
+		</view>
 		<view class="txt">
 			<rich-text :nodes='content'></rich-text>
 		</view>
+		<view style="height: 20rpx;background-color: #f8f8f8;width: 100%;">
+			
+		</view>
+		
 
-		<!-- 华丽丽的分割线 -->
+		<!-- 华丽丽的分割线
 		<view class="one_line">
 		</view>
 
@@ -35,17 +42,17 @@
 				<text>用户评价</text>
 			</view>
 			<!-- 用户评价 -->
-			<view class="toux" v-for='item in pingjia'>
+			<view class="toux" v-for="(item,index) in pingjia">
 				<view class="imgBox_a">
-					<image  mode=""></image>
+					<image mode="" :src="item.userDTO.headimg"></image>
 				</view>
 				<view class="mingc">
-					<text></text>
+					<text>{{item.userDTO.nickname}}</text>
 					<view class="time">
-						<text>2019/05/05</text>
+						<text>{{item.userDTO.birth}}</text>
 					</view>
 					<view class="huay">
-						<text>性价比高，做工精致，质量很好，性价比高，做工精致材质纯正质量很好，性价比高，做工精致</text>
+						<text>{{item.content}}</text>
 					</view>
 				</view>
 			</view>
@@ -61,7 +68,8 @@
 		<view style='position:fixed;top:0;left:0;bottom:0;right:0;background:rgba(0,0,0,0.5);z-index:99999;' v-if='pinglun'>
 			<view style='width:600rpx;height:600rpx;background:#fff;border-radius:10rpx;position:absolute;left:0;top:0;right:0;bottom:0;margin:auto;'>
 				<view style='text-align:center;margin-top:20rpx;'>添加评论</view>
-				<textarea placeholder='请输入评论' style='width:500rpx;margin:0 auto;height:300rpx;border:1px solid #eee;margin-top:20rpx;' v-model='pinglunneirong'></textarea>
+				<textarea placeholder='请输入评论' style='width:500rpx;margin:0 auto;height:300rpx;border:1px solid #eee;margin-top:20rpx;'
+				 v-model='pinglunneirong'></textarea>
 				<view style='overflow:hidden;width:400rpx;margin:10rpx auto;'>
 					<button style='color:#666;width:45%;float:left;max-width:45%;' @tap='quxiaopinglun'>取消</button>
 					<button style='color:#fff;background:#007aff;width:45%;float:right;max-width:45%;' @tap='tianjia'>确定</button>
@@ -73,56 +81,73 @@
 </template>
 
 <script>
-	export default{
-		data(){
-			return{
-				title:'',
-				create_time:'',
-				description:'',
-				content:'',
-				pingjia:[],
-				pinglun:false,
-				pinglunneirong:'',
-				newsid:''
+	export default {
+		data() {
+			return {
+				title: '',
+				create_time: '',
+				description: '',
+				content: '',
+				pingjia: [],
+				pinglun: false,
+				pinglunneirong: '',
+				newsid: '',
+				image:''
 			}
 		},
-		onLoad:function(option){
-			this.newsid=option.id
-			var _this=this
-			this.$https({url:'/api/news/article-detail',data:{id:option.id},dengl:false,success:function(res){
-				_this.title=res.data.data.article.title
-				_this.create_time=res.data.data.article.create_time
-				_this.description=res.data.data.article.description
-				_this.content=res.data.data.article.content
-				_this.pingjia=res.data.data.comList
-			}})
+		onLoad: function(option) {
+			this.newsid = option.id
+			var _this = this
+			this.$https({
+				url: '/api/news/article-detail',
+				data: {
+					id: option.id
+				},
+				dengl: false,
+				success: function(res) {
+					console.log(res.data.data)
+					_this.title = res.data.data.article.title
+					_this.create_time = res.data.data.article.create_time
+					_this.description = res.data.data.article.description
+					_this.content = res.data.data.article.content
+					_this.image = res.data.data.article.image
+					_this.pingjia = res.data.data.commList
+				}
+			})
 		},
-		methods:{
-			tianj:function(){
-				this.pinglun=true
+		methods: {
+			tianj: function() {
+				this.pinglun = true
 			},
-			tianjia:function(){
-				var _this=this
-				this.$https({url:'/api/news/article-comm-add',data:JSON.stringify({content:this.pinglunneirong,newsId:this.newsid}),dengl:false,haeder:true,method:'POST',success:function(res){
-					if(res.data.code==0){
-						uni.showToast({
-							title:res.data.message
-						})
-						_this.pinglun=false
+			tianjia: function() {
+				var _this = this
+				this.$https({
+					url: '/api/news/article-comm-add',
+					data:{
+						content: _this.pinglunneirong,
+						newsId: _this.newsid
+					},
+					dengl: false,
+					method: 'POST',
+					success: function(res) {
+						// console.log(res)
+						if (res.data.code == 0) {
+							uni.showToast({
+								title: res.data.message
+							})
+							_this.pinglun = false
+						}
 					}
-				}})
+				})
 			},
-			quxiaopinglun:function(){
-				this.pinglun=false
+			quxiaopinglun: function() {
+				this.pinglun = false
 			}
 		}
 	}
 </script>
 
 <style lang="scss">
-	
-
-
 	.top {
 		width: 750upx;
 		margin: 0 auto;
@@ -210,7 +235,7 @@
 		line-height: 70upx;
 		color: #333;
 	}
-	
+
 	.toux {
 		padding-top: 20upx;
 		padding-left: 20upx;
@@ -218,42 +243,49 @@
 		overflow: hidden;
 		border-bottom: 1px solid #ccc;
 		padding-bottom: 20upx;
+
 		image {
 			width: 80upx;
 			height: 80upx;
 			border-radius: 150upx;
 		}
 	}
-	
+
 	.evaluate {
 		width: 750upx;
 		margin-bottom: 100upx;
 	}
-	.mingc{
+
+	.mingc {
 		float: left;
 		width: 630upx;
-		text{
+
+		text {
 			font-size: 26upx;
 			padding-left: 20upx;
 		}
-		.time{
+
+		.time {
 			float: right;
 			color: #333;
 			font-size: 26upx;
 			line-height: 50upx;
 		}
 	}
-	.imgBox_a{
+
+	.imgBox_a {
 		float: left;
 		margin-top: 10upx;
 	}
-	.huay{
+
+	.huay {
 		width: 630upx;
 		float: left;
 		color: #999;
 		line-height: 40upx;
 	}
-	.publish{
+
+	.publish {
 		background: #2b5cff;
 		position: fixed;
 		bottom: 0upx;
@@ -261,9 +293,10 @@
 		width: 750upx;
 		height: 100upx;
 		overflow: hidden;
-		.imgBox_b{
-			image{
-				width:38upx ;
+
+		.imgBox_b {
+			image {
+				width: 38upx;
 				height: 38upx;
 				float: left;
 				padding-left: 38%;
@@ -271,12 +304,12 @@
 				padding-right: 20upx;
 			}
 		}
-		.textBox_b{
+
+		.textBox_b {
 			font-size: 30upx;
 			line-height: 90upx;
 			color: #fff;
 			float: left;
 		}
 	}
-	
 </style>
