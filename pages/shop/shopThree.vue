663ed9bb8297f87776deec51c875041f4b12e310
yuyanan <1431796149@qq.com>
2020-12-09 15:27:37 +0800

    新店铺

--- pages/shop/shopThree.vue
+++ pages/shop/shopThree.vue
@@ -2,7 +2,19 @@
 	<view>
 		<view class="shop-top">
 			<view class="shop-bag">
-				<image src="../../static/shopBagTwo.png" mode=""></image>
+				<swiper class="logo-swper-list" :current="currentIndex" :circular="true" :duration="100" @change="swierChange">
+					<swiper-item class="logo-swper-item" v-for="(item,i) in 5" :key="i">
+						<image src="../../static/shopBagThree.png" mode=""></image>
+					</swiper-item>
+				</swiper>
+				<view class="swper-item-icon">
+					<text>高分好店</text><text>4.8分</text>
+				</view>
+				<div class="logo-swper-dots">
+					<div v-for="(item,i) in 5" class="dots-item">
+						<div :class="i==currentIndex?'add-dots':'move-dots'"></div>
+					</div>
+				</div>
 			</view>
 			<view class="shop-search">
 				<view class="shop-search-back">
@@ -12,20 +24,24 @@
 					<input type="text" placeholder="请输入关键字" />
 				</view>
 			</view>
-			<view class="shop-logo-swper">
-				<swiper class="logo-swper-list" :current="currentIndex" :circular="true" next-margin="95rpx" :duration="100"
-				 @change="swierChange">
-					<swiper-item class="logo-swper-item" v-for="(item,i) in 5" :key="i">
-						<image src="../../static/Bitmap.png" class="slide-image" :class="currentIndex === i?'active':''" mode=""></image>
-					</swiper-item>
-				</swiper>
-				<div class="logo-swper-dots">
-					<div v-for="(item,i) in 5" class="dots-item">
-						<div :class="i==currentIndex?'add-dots':'move-dots'"></div>
-					</div>
-				</div>
+		</view>
+
+		<view class="shop-company">
+			<view class="shop-company-name">
+				<view class="company-name-title">
+					公司简介
+				</view>
+				<view class="company-name-right">
+					<image src="../../static/icon_26.png" mode=""></image>
+				</view>
+			</view>
+			<view class="shop-company-content">
+				<text>
+					公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介
+				</text>
 			</view>
 		</view>
+
 		<view class="shop-discount">
 			<view class="shop-discount-top">
 				<view class="discount-top-name">
@@ -74,21 +90,8 @@
 				</scroll-view>
 			</view>
 		</view>
-		<view class="shop-company">
-			<view class="shop-company-name">
-				<view class="company-name-title">
-					公司简介
-				</view>
-				<view class="company-name-right">
-					<image src="../../static/icon_26.png" mode=""></image>
-				</view>
-			</view>
-			<view class="shop-company-content">
-				<text>
-					公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介公司简介
-				</text>
-			</view>
-		</view>
+
+
 		<view class="shop-classify">
 			<scroll-view class="shop-classify-scroll" scroll-x="true" style="width: 100%;">
 				<view class="classify-scroll-item" v-for="(item,i) in 5">这是分类</view>
@@ -98,23 +101,26 @@
 		<view class="shop-hort">
 			<view class="hort-title">
 				<view class="hort-title-left">
-					热门产品
+					<text class="text-line" style="padding-right: 8rpx;">——</text>
+					<text style="display: inline-block;position: relative;">热门商品<text class="text-yuan"></text></text>
+					<text class="text-line" style="padding-left: 8rpx;">——</text>
 				</view>
-				<view class="hort-title-right">
-					热
-				</view>
+
 			</view>
 			<view class="hort-list">
 				<view class="hort-list-item" v-for="(item,i) in 5">
 					<view class="list-item-logo">
+						<image class="logo-border" src="../../static/bckBorder.png" mode=""></image>
 						<image src="../../static/Bitmap.png" mode=""></image>
 					</view>
 					<view class="list-item-right">
-						<view class="item-right-title">
-							商品名称商品名称商品名称
-						</view>
-						<view class="item-right-spec" v-if="false">
-							商品规格
+						<view class="item-title">
+							<view class="hort-title-right">
+								热
+							</view>
+							<view class="item-right-title">
+								商品名称商品名称商品名称
+							</view>
 						</view>
 						<view class="item-right-bottom">
 							<view class="item-right-price">
@@ -162,21 +168,85 @@
 
 	.shop-top {
 		.shop-bag {
-			position: absolute;
+			position: relative;
 			top: 0;
 			left: 0;
 			right: 0;
 
+			swiper {
+				height: 470rpx;
+			}
+
 			image {
 				width: 100%;
-				height: 450rpx;
+				height: 100%;
+
 			}
+
+
+			.swper-item-icon {
+				position: absolute;
+				color: #000f20;
+				top: 190rpx;
+				left: 40rpx;
+				height: 50rpx;
+				line-height: 50rpx;
+				width: 245rpx;
+				font-size: 24rpx;
+				border-radius: 10rpx;
+				overflow: hidden;
+
+				text:first-child {
+					display: inline-block;
+					background: #f5d14a;
+					width: 156rpx;
+					text-align: center;
+				}
+
+				text:last-child {
+					text-align: center;
+					background: #fff;
+					display: inline-block;
+					width: calc(100% - 156rpx);
+				}
+			}
+
+			.logo-swper-dots {
+				overflow: hidden;
+				position: absolute;
+				bottom: 40rpx;
+				right: 40rpx;
+
+				.dots-item {
+					float: left;
+					margin-right: 6rpx;
+				}
+
+				.move-dots {
+
+					width: 10rpx;
+					height: 10rpx;
+					border-radius: 50%;
+					background-color: #edebe9;
+
+				}
+
+				.add-dots {
+					width: 30rpx;
+					height: 12rpx;
+					background: #f4cc3d;
+					border-radius: 8rpx;
+				}
+			}
+
+
 		}
 
 		.shop-search {
-			position: relative;
+			position: absolute;
 			overflow: hidden;
 			margin-top: 90rpx !important;
+			top: 0;
 
 			.shop-search-back {
 				float: left;
@@ -210,79 +280,11 @@
 			}
 		}
 
-		.shop-logo-swper {
-			width: 100%;
-			margin-left: 30rpx;
-			position: relative;
-			top: 40rpx;
-			box-sizing: border-box;
-
-			.logo-swper-dots {
-				overflow: hidden;
-				position: absolute;
-				bottom: 20rpx;
-				right: 150rpx;
-
-				.dots-item {
-					float: left;
-					margin-right: 6rpx;
-				}
-
-				.move-dots {
-
-					width: 10rpx;
-					height: 10rpx;
-					border-radius: 50%;
-					background-color: #edebe9;
-
-				}
-
-				.add-dots {
-					width: 30rpx;
-					height: 12rpx;
-					background: #ff6600;
-					border-radius: 8rpx;
-				}
-			}
-
-
-
-			.logo-swper-list {
-				// width: 625rpx;
-				height: 352rpx;
-				// border-radius: 20rpx;
-				overflow: hidden;
-
-				.slide-image {
-					position: absolute;
-					height: 300rpx;
-					width: 530rpx;
-					opacity: 0.7;
-					margin-right: 20rpx;
-					border-radius: 20rpx;
-					overflow: hidden;
-					top: 52rpx;
-				}
-
-				.active {
-					width: 625rpx;
-					height: 352rpx;
-					opacity: 1;
-					z-index: 10;
-					border-radius: 20rpx;
-					overflow: hidden;
-					top: 0;
-
-					transition: all 0.2s ease-in 0s;
-				}
-
-			}
-		}
 	}
 
 	.shop-discount {
 		background-color: #fff;
-		padding: 90rpx 30rpx 20rpx 30rpx;
+		padding: 35rpx 30rpx 20rpx 30rpx;
 
 		.shop-discount-top {
 			overflow: hidden;
@@ -428,8 +430,11 @@
 		box-sizing: border-box;
 		padding: 30rpx 40rpx 20rpx 30rpx;
 		background-color: #fff;
-		margin-top: 10rpx;
 		height: 200rpx;
+		position: relative;
+		top: -20rpx;
+		border-top-left-radius: 25rpx;
+		border-top-right-radius: 25rpx;
 
 		.shop-company-name {
 			font-size: 24rpx;
@@ -500,76 +505,113 @@
 			.hort-title-left {
 				font-size: 30rpx;
 				color: #181818;
-				float: left;
+				text-align: center;
+
+				.text-line {
+					color: #ed5d1f;
+
+				}
+
+				.text-yuan {
+					width: 10rpx;
+					height: 10rpx;
+					background: #ed5d1f;
+					display: inline-block;
+					border-radius: 50%;
+					position: absolute;
+					top: 2.5rpx;
+					right: -5px;
+				}
 			}
 
-			.hort-title-right {
-				height: 34rpx;
-				line-height: 34rpx;
-				font-size: 24rpx;
-				color: #ff4267;
-				border: 2rpx solid #ff4267;
-				box-sizing: border-box;
-				width: 48rpx;
-				text-align: center;
-				float: left;
-				border-radius: 20rpx;
-				background: #ffecf0;
-				margin-left: 12rpx;
-			}
 		}
 
 		.hort-list {
+			margin: 30rpx 28rpx;
+			overflow: hidden;
+
+			.hort-list-item:nth-child(odd) {
+				float: left;
+			}
+
+			.hort-list-item:nth-child(even) {
+				float: right;
+			}
 
 			.hort-list-item {
-				overflow: hidden;
-				padding: 20rpx 30rpx;
 				border-bottom: 1rpx solid #f6f6f6;
+				width: calc((100% - 28rpx)/2);
+				box-sizing: border-box;
+				height: 420rpx;
+				border-radius: 15rpx;
+				overflow: hidden;
+				margin-bottom: 28rpx;
+				box-shadow: 0 1px 2px 1px rgba(0,19,41,0.08);
 
 				.list-item-logo {
-					float: left;
+					width: 100%;
+					height: 296rpx;
+					box-sizing: border-box;
+					position: relative;
+
+					.logo-border {
+						position: absolute;
+					}
 
 					image {
-						width: 216rpx;
-						height: 145rpx;
-						border-radius: 15rpx;
-						overflow: hidden;
+
+						width: 100%;
+						height: 100%;
 						display: block;
 					}
 				}
 
+				.item-title {
+					padding-left: 15rpx;
+					padding-top: 20rpx;
+					overflow: hidden;
+				}
+
 				.list-item-right {
-					width: calc(100% - 216rpx);
-					float: left;
 					box-sizing: border-box;
-					padding-left: 20rpx;
+
+					.hort-title-right {
+						// padding-left: ;
+					}
+
+					.hort-title-right {
+						height: 34rpx;
+						line-height: 34rpx;
+						font-size: 24rpx;
+						color: #ff4267;
+						border: 2rpx solid #ff4267;
+						box-sizing: border-box;
+						width: 48rpx;
+						text-align: center;
+						border-radius: 20rpx;
+						background: #ffecf0;
+						float: left;
+						margin-right: 10rpx;
+					}
 
 					.item-right-title {
 						font-size: 26rpx;
+						width: calc(100% - 73rpx);
 						overflow: hidden;
 						text-overflow: ellipsis;
 						white-space: nowrap;
 					}
 
-					.item-right-spec {
-						line-height: 52rpx;
-						font-size: 22rpx;
-						color: #bcbcbc;
-						background: #f8f8f8;
-						width: fit-content;
-						padding: 0 15rpx;
-						border-radius: 6rpx;
-						margin-top: 6rpx;
-					}
 
 					.item-right-bottom {
 						overflow: hidden;
 						font-size: 24rpx;
-						margin-top:80rpx;
+						margin-top:18rpx;
 
 						.item-right-price {
 							float: left;
 							color: #ed5d1f;
+							padding-left: 15rpx;
 
 							text {
 								font-size: 11px;
@@ -583,6 +625,7 @@
 							color: #9b9b9b;
 							margin-left: 35rpx;
 							float: left;
+							font-size: 20rpx;
 						}
 					}
 				}
