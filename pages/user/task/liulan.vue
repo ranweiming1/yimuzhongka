b7eed1c8134dc130df53f8931042224fb3729ead
yuyanan <1431796149@qq.com>
2020-12-16 14:00:11 +0800

    修改浏览记录样式

--- pages/user/task/liulan.vue
+++ pages/user/task/liulan.vue
@@ -1,17 +1,27 @@
 <template>
 	<view class="">
-		<view class="hahah list uni-flex uni-column">
-			<view class="content " v-for="(item,index) in lList" @tap="detail(item.goodsId)">
+		<view class="clearCss">
+			<view class="content-item " v-for="(item,index) in lList" @tap="detail(item.goodsId)">
 				<view class="imgBox">
-					<image :src="item.goodsLogo" mode="widthFix"></image>
+					<image :src="item.goodsLogo" mode=""></image>
 				</view>
-				<view class="txt_a">
-					<text class="span_a" v-if="item.selfStatus=='Y'">自营</text>
-					<text>{{item.goodsName}}</text>
-					<view class="txt_aa" v-for="(items,indexs) in item.couponDTOS">
-						<text>满{{items.condition}}-{{items.money}}元<</text> </view> <view class="txt_aas">
-								<text>税后价：<text>{{item.marketPrice?'￥'+item.marketPrice:'暂无价格'}}</text></text>
-								<text>销量：{{item.salesSum}}</text>
+				<view class="content-item-text">
+					<view class="title_top">
+						<text class="span_a" v-if="item.selfStatus=='Y'">自营</text>						
+						<text class="titleText">{{item.goodsName}}</text>
+					</view>
+					<view class="item-coupon">
+						<view class="coupon-item" v-for="(items,indexs) in item.couponDTOS">
+							<text>满{{items.condition}}-{{items.money}}元</text>
+						</view>
+						<view class="coupon-item" v-if='item.kuaidi==0'>
+							<text>包邮</text>
+						</view>
+
+					</view>
+					<view class="item-price">
+						<text class="price-text">￥{{item.marketPrice?item.marketPrice.toFixed(2):'暂无价格'}}</text>
+						<text class="shop-sales">销量：{{item.salesSum}}</text>
 					</view>
 
 				</view>
@@ -50,9 +60,8 @@
 						goodsId: id
 					},
 					method: 'POST',
-					dengl:true,
-					success(res) {
-					}
+					dengl: true,
+					success(res) {}
 				})
 			}
 		}
@@ -60,27 +69,29 @@
 </script>
 
 <style lang="scss">
+	.shop-sales {
+		color: #999999;
+		font-size:22upx;
+		float: right;
+		
+	}
 	.content {
 		display: black;
 		width: 710upx;
 		padding: 20upx;
 		border-bottom: 1px dotted #ccc;
-		padding-bottom: 20upx;
 		overflow: hidden;
 
-		.imgBox {
-			image {
-				width: 30%;
-				height: inherit;
-				float: left;
-			}
-		}
+
 
 		.txt_a {
 			padding-top: 10upx;
-			width: 67%;
-			margin-left: 20upx;
+			width: calc(100% - 200rpx);
+			padding-left: 20rpx;
+			box-sizing: border-box;
 			float: left;
+			height: 200rpx;
+			position: relative;
 
 			text {
 				font-size: 30upx;
@@ -111,7 +122,10 @@
 			}
 
 			.txt_aas {
-				padding-top: 10upx;
+				position: absolute;
+				bottom: 0;
+				width: calc(100% - 20rpx);
+				box-sizing: border-box;
 
 				text {
 					color: #333;
@@ -136,7 +150,8 @@
 
 		}
 	}
-.content:first-child{
-	border-top: 1px solid #ccc;
-}
+
+	.content:first-child {
+		border-top: 1px solid #ccc;
+	}
 </style>
